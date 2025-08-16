1029d3a80511c875f23ac1d3892d116e5c1372ab
Satyam Gagare <satyamgagre4@gmail.com>
2025-08-16 16:48:48 +0530

    16-08-2025

---
+++ DS_USING_PYTHON/6.py
@@ -0,0 +1,19 @@
+#Python modules
+# importing the math module
+import math
+
+# accessing a variable from the math module
+print(math.pi)
+
+# calling a funtion from a math module
+print(math.sqrt(5))
+
+# ---------------------------------------------------------------------------
+
+from random import  randint, choice
+
+# calling the randit() function directly
+print(randint(1, 10))
+
+# calling the choice() fucntion directly
+print(choice(["apple","orange","banana","dragon fruit","cherry"]))
