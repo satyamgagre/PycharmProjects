c04ea6f039e57bf158e5f20439861f10264099cf
Satyam Gagare <satyamgagre4@gmail.com>
2025-08-16 18:49:00 +0530

    16-08-2025

---
+++ DS_USING_PYTHON/9.py
@@ -0,0 +1,14 @@
+#Lambda functions
+# Lambda functions also known as anonymous functions.
+# 1] Add two numbers
+add = lambda x, y: x + y
+result = add(10, 20)
+
+print(result)
+
+# 2] Square a number
+square = lambda x: x ** x
+result = square(10)
+print(result)
+
+
